651037d5e3319cad4427d06a3756c456d956f22d
Hackmoo <hackmo872@gmail.com>
2023-04-16 21:56:38 +0300

    backend is done

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express, { request } from 'express'
-import mongoose from 'mongoose'
-import { registerValidation, loginValidation, postCreateValidation } from './validations/auth.js'
-import checkAuth from './utilities/checkAuth.js'
 
-import * as userController from './controllers/userController.js'
-import * as postController from './controllers/postController.js'
+import mongoose from 'mongoose'
+
+import { registerValidation, loginValidation, postCreateValidation } from './validations/auth.js'
+
+import multer from 'multer'
+
+import {postController, userController} from './controllers/index.js'
+import {ValidationErrors, checkAuth} from './utilities/index.js'
+
 
 mongoose.connect('mongodb+srv://admin:admin@mern-test.ado02wn.mongodb.net/blog?retryWrites=true&w=majority') // подключение mongoDB к проету
 .then(() => console.log('DB is OK'))
@@ -12,16 +16,35 @@ mongoose.connect('mongodb+srv://admin:admin@mern-test.ado02wn.mongodb.net/blog?r
 
 const app = express()  // Инициализация Express JS
 
-app.use(express.json()) // Позволяет читать JSON для express
+const storage = multer.diskStorage({
+  destination: (_, a, cb) => {
+    cb(null, 'uploads')
+  },
+  filename: (_, file,cb) => {
+    cb(null, file.originalname)
+  },
+})
 
-app.post('/login', loginValidation, userController.login)
-app.post('/register', registerValidation , userController.register)
+const upload = multer({ storage })
+
+app.use(express.json()) // Позволяет читать JSON для express
+app.use('/uploads', express.static('uploads')) 
+
+app.post('/login', loginValidation, ValidationErrors, userController.login)
+app.post('/register', registerValidation, ValidationErrors, userController.register)
+app.post('/posts', checkAuth,postCreateValidation, ValidationErrors,postController.create)
+
+app.post('/upload', checkAuth,upload.single('image'), (req,res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`
+  })
+})
+
 app.get('/posts', postController.getAll)
 app.get('/posts/:id', postController.getOne)
-app.post('/posts', checkAuth,postCreateValidation,postController.create)
 app.get('/me', checkAuth, userController.getMe)
 app.delete('/posts/:id', checkAuth,postController.remove)
-app.patch('/posts/:id', checkAuth,postController.update)
+app.patch('/posts/:id', checkAuth,postCreateValidation,postController.update)
 
 app.listen(7777, (err) => {   // Установка порта сервера, и проверка на ошибки
   if(err) return console.log(err)
